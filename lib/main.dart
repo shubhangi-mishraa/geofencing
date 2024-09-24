718dec152e2dde8934c97bf687a40975f9c5b3ca
Shubhangi Mishra <sonalestilo@gmail.com>
2024-09-25 00:21:37 +0530

    Check-in check-out working

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:geofence_service/geofence_service.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() => runApp(const CheckInApp());
 
@@ -13,49 +17,106 @@ class CheckInApp extends StatefulWidget {
 }
 
 class _CheckInAppState extends State<CheckInApp> {
-  final _activityStreamController = StreamController<Activity>();
   final _geofenceStreamController = StreamController<Geofence>();
-
-  // Store check-in/out records
-  final List<String> _records = [];
-
-  // Create a GeofenceService instance
+  final List<Map<String, dynamic>> _records = [];
   final _geofenceService = GeofenceService.instance.setup(
     interval: 5000,
     accuracy: 100,
     loiteringDelayMs: 60000,
     statusChangeDelayMs: 10000,
-    useActivityRecognition: true,
+    useActivityRecognition: false,
     allowMockLocations: false,
     printDevLog: false,
   );
 
-  late List<Geofence> _geofenceList;
+  late List<Geofence> _geofenceList = []; 
+  double? _distanceFromGeofence;
+  String? _currentCoordinates;
 
   @override
   void initState() {
     super.initState();
-    _initializeGeofences();
+    checkPermissionStatus();
+    _loadGeofences(); 
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _geofenceService.addGeofenceStatusChangeListener(_onGeofenceStatusChanged);
-      _geofenceService.addActivityChangeListener(_onActivityChanged);
+      _geofenceService
+          .addGeofenceStatusChangeListener(_onGeofenceStatusChanged);
       _geofenceService.addStreamErrorListener(_onError);
       _geofenceService.start(_geofenceList).catchError(_onError);
     });
+
+    _getUserDistanceFromGeofence();
   }
 
-  void _initializeGeofences() {
-    final data = json.decode(locationsJson);
-    _geofenceList = (data['data'][0]['buildings'] as List).map((building) {
-      return Geofence(
-        id: building['name'],
-        latitude: building['latitude'],
-        longitude: building['longitude'],
-        radius: [
-          GeofenceRadius(id: 'radius_${building['radius']}', length: building['radius']),
-        ],
-      );
-    }).toList();
+  Future<void> getPermission() async {
+    if (await Permission.location.request().isGranted) {
+    }
+  }
+
+  Future<void> checkPermissionStatus() async {
+    var status = await Permission.location.status;
+    if (status.isDenied) {
+      getPermission();
+    }
+  }
+
+  Future<void> _loadGeofences() async {
+    final String response = await rootBundle.loadString('assets/location.json');
+    final data = json.decode(response);
+
+    for (var building in data['data'][0]['buildings']) {
+      double radiusValue = building['radius']?.toDouble() ?? 0.0;
+      if (radiusValue > 0.0) {
+        _geofenceList.add(Geofence(
+          id: building['name'],
+          latitude: building['latitude'],
+          longitude: building['longitude'],
+          radius: [
+            GeofenceRadius(
+                id: 'radius_${building['radius']}', length: radiusValue),
+          ],
+        ));
+      } else {
+        print('Invalid radius for building: ${building['name']}');
+      }
+
+      for (var asset in building['assets']) {
+        double assetRadiusValue = asset['radius']?.toDouble() ?? 0.0;
+        if (assetRadiusValue > 0.0) {
+          _geofenceList.add(Geofence(
+            id: asset['assetsName'],
+            latitude: asset['latitude'],
+            longitude: asset['longitude'],
+            radius: [
+              GeofenceRadius(
+                  id: 'radius_${asset['radius']}', length: assetRadiusValue),
+            ],
+          ));
+        } else {
+          print('Invalid radius for asset: ${asset['assetsName']}');
+        }
+      }
+    }
+
+    _geofenceService.start(_geofenceList).catchError(_onError);
+  }
+
+  Future<void> _getUserDistanceFromGeofence() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition();
+      setState(() {
+        _currentCoordinates =
+            'Lat: ${position.latitude}, Lng: ${position.longitude}';
+      });
+
+      if (_geofenceList.isNotEmpty) {
+        final geofence = _geofenceList[0];
+        _calculateDistanceFromGeofence(position.latitude, position.longitude,
+            geofence.latitude, geofence.longitude);
+      }
+    } catch (e) {
+      print('Error fetching user location: $e');
+    }
   }
 
   Future<void> _onGeofenceStatusChanged(
@@ -63,23 +124,42 @@ class _CheckInAppState extends State<CheckInApp> {
       GeofenceRadius geofenceRadius,
       GeofenceStatus geofenceStatus,
       Location location) async {
-    final currentTime = DateTime.now().toString();
-    String record;
+    final currentTime = DateTime.now();
+    String formattedTime =
+        DateFormat('dd/MM/yyyy HH:mm:ss').format(currentTime);
+
+    print('Geofence: ${geofence.id}, Status: $geofenceStatus');
+
     if (geofenceStatus == GeofenceStatus.ENTER) {
-      record = 'Checked in to ${geofence.id} at $currentTime';
+      setState(() {
+        _records.add({
+          'status': 'Checked in',
+          'geofenceId': geofence.id,
+          'time': formattedTime,
+        });
+        _distanceFromGeofence = null; 
+      });
     } else if (geofenceStatus == GeofenceStatus.EXIT) {
-      record = 'Checked out of ${geofence.id} at $currentTime';
-    } else {
-      return;
+      setState(() {
+        _records.add({
+          'status': 'Checked out',
+          'geofenceId': geofence.id,
+          'time': formattedTime,
+        });
+        _calculateDistanceFromGeofence(location.latitude, location.longitude,
+            geofence.latitude, geofence.longitude);
+      });
     }
-    setState(() {
-      _records.add(record);
-    });
-    print(record);
   }
 
-  void _onActivityChanged(Activity prevActivity, Activity currActivity) {
-    print('Activity changed: ${currActivity.toJson()}');
+  void _calculateDistanceFromGeofence(double userLat, double userLng,
+      double geofenceLat, double geofenceLng) async {
+    final distance =
+        Geolocator.distanceBetween(userLat, userLng, geofenceLat, geofenceLng);
+
+    setState(() {
+      _distanceFromGeofence = distance;
+    });
   }
 
   void _onError(error) {
@@ -88,7 +168,6 @@ class _CheckInAppState extends State<CheckInApp> {
 
   @override
   void dispose() {
-    _activityStreamController.close();
     _geofenceStreamController.close();
     super.dispose();
   }
@@ -96,79 +175,86 @@ class _CheckInAppState extends State<CheckInApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Check-In/Out App'),
           centerTitle: true,
         ),
-        body: ListView.builder(
-          itemCount: _records.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(_records[index]),
-            );
-          },
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                _currentCoordinates ?? 'Fetching coordinates...',
+                style: TextStyle(fontSize: 16, color: Colors.blue),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _records.length,
+                itemBuilder: (context, index) {
+                  final record = _records[index];
+                  return Card(
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    elevation: 4,
+                    child: ListTile(
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: '${record['status']} ',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: record['status'] == 'Checked in'
+                                        ? Colors.green
+                                        : Colors.red,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: '${record['geofenceId']} \n',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.black),
+                                ),
+                                TextSpan(
+                                  text: record['time'],
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            if (_distanceFromGeofence != null)
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'You are ${_distanceFromGeofence!.toStringAsFixed(2)} meters away from the geofence.',
+                  style: TextStyle(color: Colors.red, fontSize: 16),
+                ),
+              ),
+          ],
         ),
       ),
     );
   }
 }
-const String locationsJson = '''
-{
-  "data": [
-    {
-      "buildings": [
-        {
-          "id": 1,
-          "name": "Building 1(Blossom Heights)",
-          "latitude": 17.4470042,
-          "longitude": 78.3810862,
-          "radius": 10.0,
-          "radiusUnit": "meter",
-          "assets": [
-            {
-              "id": 101,
-              "assetsName": "Assets 1(Conference Hall)",
-              "latitude": 17.4470362,
-              "longitude": 78.3811563,
-              "radius": 1.0
-            },
-            {
-              "id": 102,
-              "assetsName": "Assets 2(RK Sir Cabin)",
-              "latitude": 17.447003,
-              "longitude": 78.3811143,
-              "radius": 1.0
-            },
-            {
-              "id": 103,
-              "assetsName": "Assets 3(MD Cabin)",
-              "latitude": 17.4469637,
-              "longitude": 78.3811912,
-              "radius": 0.0
-            }
-          ]
-        },
-        {
-          "id": 1,
-          "name": "Building 2(Blossom Heights)",
-          "latitude": 28.6868032,
-          "longitude": 77.3386944,
-          "radius": 10.0,
-          "radiusUnit": "meter",
-          "assets": [
-            {
-              "id": 101,
-              "assetsName": "Assets 1(Conference Hall)",
-              "latitude": 28.5868032,
-              "longitude": 77.3586944,
-              "radius": 1.0
-            }
-            
-          ]
-        }
-      ]
-    }
-  ]
-}
-''';
